be4fa2325018cc926f5053cf67140d2ea2b8e899
CaiSijian <15778535572@139.com>
2023-12-13 04:28:11 +0800

    feat:做好了基本的配置，开始写header

---
+++ src/components/navbar-wrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="logo-container">
+        <el-link href="/" type="warning" :underline="false">
+            <img :src="logo">
+            <span>匣子心理</span>
+        </el-link>
+    </div>
+</template>
+
+<script setup lang="ts">
+import logo from '~/assets/logo.svg'
+</script>
+
+<style scoped lang="scss">
+.logo-container {
+    display: flex;
+    align-items: center;
+    height: var(--header-height);
+
+    img {
+        width: 1.8rem;
+    }
+}
+</style>
