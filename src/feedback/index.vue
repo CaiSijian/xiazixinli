e827bfbf038d4dcd3ec6ff8e5555f8e234281577
CaiSijian <15778535572@139.com>
2023-12-31 23:18:15 +0800

    feat:基本完成

--- src/feedback/index.vue
+++ src/feedback/index.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>feedback</div>
+    <iframe src="https://f.kdocs.cn/g/zxodyZBU/" frameborder="0"></iframe>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+iframe {
+    /* iframes are inline by default */
+    border: none;
+    /* Reset default border */
+    /* Viewport-relative units */
+    height: calc(100vh - 55px - 49px);
+    width: 100vw;
+
+    @media screen {
+        @media (min-width: @global-breakpoint) {
+            height: calc(100vh - 55px);
+        }
+    }
+}
+</style>
